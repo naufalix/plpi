541e231a4261e9628b12f14e269a66649686a77f
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2022-12-09 10:14:40 +0700

    Feat: CRUD Career

---
+++ app/Http/Controllers/Dashboard/DashCareer.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+use App\Http\Controllers\Controller;
+use App\Models\Career;
+use App\Models\Meta;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class DashCareer extends Controller
+{
+
+    public function previlege($p){
+        $auth = Auth::guard('user')->user();
+        $previlege = explode(",",$auth->previlege);
+        if(!in_array($p, $previlege)){ return false; }
+        return true;
+    }
+    
+    public function index(){
+        if(!$this->previlege(6)){
+            return redirect('/dashboard/home')->with("info","Anda tidak punya akses");
+        }
+        $meta = Meta::$data_meta;
+        $meta['title'] = 'Dashboard | Data karir';
+        return view('dashboard.career',[
+            "meta" => $meta,
+            "careers" => Career::with(['user'])->get(),
+            "users" => User::all(),
+        ]);
+    }
+
+    public function postHandler(Request $request){
+        $this->previlege(6);
+        if($request->submit=="store"){
+            $res = $this->store($request);
+            return redirect('/dashboard/career')->with($res['status'],$res['message']);
+        }
+        if($request->submit=="update"){
+            $res = $this->update($request);
+            return redirect('/dashboard/career')->with($res['status'],$res['message']);
+        }
+        if($request->submit=="destroy"){
+            $res = $this->destroy($request);
+            return redirect('/dashboard/career')->with($res['status'],$res['message']);
+        }else{
+            return redirect('/dashboard/career')->with("info","Submit not found");
+        }
+    }
+
+    public function store(Request $request){
+        $validatedData = $request->validate([
+            'user_id'=>'required|numeric',
+            'position' => 'required',
+            'rank' => 'required',
+            'start_date'=>'required',
+            'end_date'=>'required',
+        ]);
+
+        // Create Career
+        Career::create($validatedData);
+        return ['status'=>'success','message'=>'Karir berhasil ditambahkan']; 
+    }
+
+    public function update(Request $request){
+        $validatedData = $request->validate([
+            'id'=>'required|numeric',
+            'user_id'=>'required|numeric',
+            'position' => 'required',
+            'rank' => 'required',
+            'start_date'=>'required',
+            'end_date'=>'required',
+        ]);
+        
+        //Check if career is found
+        if(Career::find($request->id)){
+            //Check user
+            if(!User::find($request->user_id)){
+                return ['status'=>'error','message'=>'User tidak ditemukan'];
+            }
+            // Update career
+            Career::find($request->id)->update($validatedData);   
+            return ['status'=>'success','message'=>'Karir berhasil diedit']; 
+        }else{
+            return ['status'=>'error','message'=>'Karir tidak ditemukan'];
+        }
+    }
+
+    public function destroy(Request $request){
+        
+        $validatedData = $request->validate([
+            'id'=>'required|numeric',
+        ]);
+
+        //Check if the career is found
+        if(Career::find($request->id)){
+            Career::destroy($request->id);    // Delete career
+            return ['status'=>'success','message'=>'Karir berhasil dihapus'];
+        }else{
+            return ['status'=>'error','message'=>'Karir tidak ditemukan'];
+        }
+    }
+}
---
+++ resources/views/dashboard/career.blade.php
@@ -0,0 +1,202 @@
+@extends('layouts.admin')
+
+@section('content')
+<!--begin::Section-->
+<div>
+  <!--begin::Heading-->
+  <div class="col-12 d-flex">
+    <h1 class="anchor fw-bolder mb-5" id="striped-rounded-bordered">Data Karir</h1>
+    <button class="ms-auto btn btn-primary" data-bs-toggle="modal" data-bs-target="#tambah">Tambah karir</button>
+  </div>
+  <!--end::Heading-->
+  <!--begin::Block-->
+  <div class="my-5 table-responsive">
+    <table id="myTable" class="table table-striped table-hover table-rounded border gs-7" style="font-size: 13px">
+      <thead>
+        <tr class="fw-bold fs-6 text-gray-800 border-bottom border-gray-200">
+          <th>No</th>
+          <th>Nama User</th>
+          <th>Jabatan</th>
+          <th>Pangkat</th>
+          <th>Tanggal Mulai</th>
+          <th>Tanggal Selesai</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($careers as $career)
+        @php
+          $start_date = date_create($career->start_date);
+          $end_date = date_create($career->end_date);
+        @endphp
+        <tr>
+          <td>{{ $loop->iteration }}</td>
+          <td style="min-width: 320px;">{{ $career->user->name }}  </td>
+          <td style="min-width: 100px;"><span class="badge badge-primary">{{ $career->position }}</span></td>
+          <td style="min-width: 100px;"><span class="badge badge-success">{{ $career->rank }}</span></td>
+          <td>{{ date_format($start_date,"d F Y") }}</td>
+          <td>{{ date_format($end_date,"d F Y") }}</td>
+          <td style="min-width: 100px;">
+            <a href="#" class="btn btn-icon btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#edit" onclick="edit({{ $career->id }})"><i class="bi bi-pencil-fill"></i></a>
+            <a href="#" class="btn btn-icon btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#hapus" onclick="hapus({{ $career->id }})"><i class="bi bi-x-lg"></i></a>
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+  <!--end::Block-->
+</div>
+<!--end::Section-->
+
+<div class="modal fade" tabindex="-1" id="tambah">
+  <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h3 class="modal-title">Tambah Karir</h3>
+          <div class="btn btn-icon btn-sm btn-active-light-primary ms-2" data-bs-dismiss="modal" aria-label="Close">
+            <i class="bi bi-x-lg"></i>
+          </div>
+        </div>
+        <form class="form" method="post" action="">
+          @csrf
+          <div class="modal-body">
+            <div class="row g-9 mb-8">
+              <div class="col-12 col-md-6">
+                <label class="required fw-bold mb-2">Nama user</label>
+                <select class="form-select form-select-solid" id="tui" name="user_id" tabindex="-1" aria-hidden="true" required>
+                  @foreach ($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                  @endforeach
+                </select>
+              </div>
+              <div class="col-12 col-md-3">
+                <label class="required fw-bold mb-2">Jabatan</label>
+                <input type="text" class="form-control form-control-solid" id="position" name="position" required>
+              </div>
+              <div class="col-12 col-md-3">
+                <label class="required fw-bold mb-2">Pangkat</label>
+                <input type="text" class="form-control form-control-solid" id="rank" name="rank" required>
+              </div>
+            </div>
+            <div class="row g-9 mb-8">
+              <div class="col-6">
+                <label class="required fw-bold mb-2">Tanggal mulai</label>
+                <input type="date" class="form-control" id="start_date" name="start_date" required>
+              </div>
+              <div class="col-6">
+                <label class="required fw-bold mb-2">Tanggal selesai</label>
+                <input type="date" class="form-control" id="end_date" name="end_date" required>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="reset" class="btn btn-light me-3" data-bs-dismiss="modal">Cancel</button>
+            <button type="submit" class="btn btn-primary" name="submit" value="store">Submit</button>
+          </div>
+        </form>
+      </div>
+  </div>
+</div>
+
+<div class="modal fade" tabindex="-1" id="edit">
+  <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h3 class="modal-title">Edit Karir</h3>
+          <div class="btn btn-icon btn-sm btn-active-light-primary ms-2" data-bs-dismiss="modal" aria-label="Close">
+            <i class="bi bi-x-lg"></i>
+          </div>
+        </div>
+        <form class="form" method="post" action="">
+          @csrf
+          <input type="hidden" class="d-none" id="eid" name="id">
+          <div class="modal-body">
+            <div class="row g-9 mb-8">
+              <div class="col-12 col-md-6">
+                <label class="required fw-bold mb-2">Nama user</label>
+                <select class="form-select form-select-solid" id="eui" name="user_id" tabindex="-1" aria-hidden="true" required>
+                  @foreach ($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                  @endforeach
+                </select>
+              </div>
+              <div class="col-12 col-md-3">
+                <label class="required fw-bold mb-2">Jabatan</label>
+                <input type="text" class="form-control form-control-solid" id="eps" name="position" required>
+              </div>
+              <div class="col-12 col-md-3">
+                <label class="required fw-bold mb-2">Pangkat</label>
+                <input type="text" class="form-control form-control-solid" id="ern" name="rank" required>
+              </div>
+            </div>
+            <div class="row g-9 mb-8">
+              <div class="col-6">
+                <label class="required fw-bold mb-2">Tanggal mulai</label>
+                <input type="date" class="form-control" id="esd" name="start_date" required>
+              </div>
+              <div class="col-6">
+                <label class="required fw-bold mb-2">Tanggal selesai</label>
+                <input type="date" class="form-control" id="eed" name="end_date" required>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="reset" class="btn btn-light me-3" data-bs-dismiss="modal">Cancel</button>
+            <button type="submit" class="btn btn-primary" name="submit" value="update">Submit</button>
+          </div>
+        </form>
+      </div>
+  </div>
+</div>
+
+<div class="modal fade" tabindex="-1" id="hapus">
+  <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h3 class="modal-title">Hapus Karir</h3>
+          <div class="btn btn-icon btn-sm btn-active-light-primary ms-2" data-bs-dismiss="modal" aria-label="Close">
+            <i class="bi bi-x-lg"></i>
+          </div>
+        </div>
+        <form class="form" method="post" action="">
+          @csrf
+          <div class="modal-body">
+            <input type="hidden" class="d-none" id="hi" name="id">
+            <label class="fw-bold mb-2" id="hd">Apakah anda yakin ingin menghapus karir ini?</label>
+          </div>
+          <div class="modal-footer">
+            <button type="reset" class="btn btn-light me-3" data-bs-dismiss="modal">Cancel</button>
+            <button type="submit" class="btn btn-danger" name="submit" value="destroy">Delete</button>
+          </div>
+        </form>
+      </div>
+  </div>
+</div>
+
+<script type="text/javascript">
+  $(document).ready(function () {
+    $('#tui').select2({  dropdownParent: $("#tambah")  });
+    $('#eui').select2({  dropdownParent: $("#edit")  });
+  });
+  function edit(id){
+    $.ajax({
+      url: "/api/career/"+id,
+      type: 'GET',
+      dataType: 'json', // added data type
+      success: function(mydata) {
+        $("#eid").val(id);
+        $("#eui").val(mydata.user_id);
+        $("#eps").val(mydata.position);
+        $("#ern").val(mydata.rank);
+        $("#esd").val(mydata.start_date);
+        $("#eed").val(mydata.end_date);
+        $('#eui').select2({  dropdownParent: $("#edit")  });
+      }
+    });
+  }
+  function hapus(id){
+    $("#hi").val(id);
+  }
+</script>
+@endsection
