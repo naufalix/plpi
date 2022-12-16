f17b337d460d7055d83df6b811efc704f2b560f4
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2022-12-16 12:54:48 +0700

    Add social links to footer

--- resources/views/partials/admin-footer.blade.php
+++ resources/views/partials/admin-footer.blade.php
@@ -3,66 +3,28 @@
             <div class="container d-flex flex-column flex-md-row align-items-center justify-content-between">
               <!--begin::Copyright-->
               <div class="text-dark order-2 order-md-1">
-                <span class="text-muted fw-bold me-1">2021©</span>
-                <a href="https://keenthemes.com" target="_blank" class="text-gray-800 text-hover-primary">Keenthemes</a>
+                <span class="text-muted fw-bold me-1">Created by </span>
+                <a href="https://naufal.dev" target="_blank" class="text-gray-800 text-hover-primary">naufal.dev</a>
               </div>
               <!--end::Copyright-->
               <!--begin::Menu-->
               <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1 align-items-center mb-3 mb-md-0">
-                <!--begin::Menu item-->
+                
                 <li class="menu-item">
                   <!--begin::Menu link-->
-                  <a href="https://www.youtube.com/c/KeenThemesTuts/videos" target="_blank" class="menu-link px-3">
-                    <img alt="Keenthemes Youtube" src="/assets/media/svg/social-logos/youtube.svg" class="h-20px" />
+                  <a href="https://github.com/naufalix" target="_blank" class="menu-link px-3">
+                    <img alt="Naufalix Github" src="/assets/media/svg/social-logos/github.svg" class="h-20px" />
                   </a>
                   <!--end::Menu link-->
                 </li>
-                <!--end::Menu link-->
-                <!--begin::Menu item-->
                 <li class="menu-item">
                   <!--begin::Menu link-->
-                  <a href="https://github.com/KeenthemesHub" target="_blank" class="menu-link px-3">
-                    <img alt="Keenthemes Github" src="/assets/media/svg/social-logos/github.svg" class="h-20px" />
+                  <a href="https://www.instagram.com/naufalix" target="_blank" class="menu-link px-3">
+                    <img alt="Naufalix Instagram" src="/assets/media/svg/social-logos/instagram.svg" class="h-20px" />
                   </a>
                   <!--end::Menu link-->
                 </li>
-                <!--end::Menu link-->
-                <!--begin::Menu item-->
-                <li class="menu-item">
-                  <!--begin::Menu link-->
-                  <a href="https://twitter.com/keenthemes" target="_blank" class="menu-link px-3">
-                    <img alt="Keenthemes Twitter" src="/assets/media/svg/social-logos/twitter.svg" class="h-20px" />
-                  </a>
-                  <!--end::Menu link-->
-                </li>
-                <!--end::Menu link-->
-                <!--begin::Menu item-->
-                <li class="menu-item">
-                  <!--begin::Menu link-->
-                  <a href="https://www.instagram.com/keenthemes" target="_blank" class="menu-link px-3">
-                    <img alt="Keenthemes Instagram" src="/assets/media/svg/social-logos/instagram.svg" class="h-20px" />
-                  </a>
-                  <!--end::Menu link-->
-                </li>
-                <!--end::Menu link-->
-                <!--begin::Menu item-->
-                <li class="menu-item">
-                  <!--begin::Menu link-->
-                  <a href="https://www.facebook.com/keenthemes" target="_blank" class="menu-link px-3">
-                    <img alt="Keenthemes Facebook" src="/assets/media/svg/social-logos/facebook.svg" class="h-20px" />
-                  </a>
-                  <!--end::Menu link-->
-                </li>
-                <!--end::Menu link-->
-                <!--begin::Menu item-->
-                <li class="menu-item">
-                  <!--begin::Menu link-->
-                  <a href="https://dribbble.com/keenthemes" target="_blank" class="menu-link px-3">
-                    <img alt="Keenthemes Dribbble" src="/assets/media/svg/social-logos/dribbble.svg" class="h-20px" />
-                  </a>
-                  <!--end::Menu link-->
-                </li>
-                <!--end::Menu link-->
+
               </ul>
               <!--end::Menu-->
             </div>
