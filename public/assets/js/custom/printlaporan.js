b2e6f11626e508f9ba77062cc10620fddb8adbb6
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2023-01-06 23:16:31 +0700

    Create dataexport.js

--- public/assets/js/custom/printlaporan.js
+++
@@ -1,9 +0,0 @@
-function printlaporan(url){
-  // MyWindow=window.open('/dashboard/logbook/print','MyWindow','width=800,height=600'); 
-  // return false;
-  if(url=='transaction'){
-    uid = $('#pui').val();
-    url = url+'?user_id='+uid;
-  }
-  window.open('/print/'+url,'POPUP WINDOW TITLE HERE','width=800,height=800').print()
-}
