ce25ac270c193735b3daf5f66a831e940bfac67b
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2023-01-06 23:17:49 +0700

    Create Toast from Sweetalert

--- resources/views/partials/script.blade.php
+++ resources/views/partials/script.blade.php
@@ -13,7 +13,15 @@
     <script src="/assets/js/custom/documentation/documentation.js"></script>
     <script src="/assets/js/custom/documentation/search.js"></script>
     <script src="/assets/js/custom/authentication/sign-in/general.js"></script>
-    <script src="/assets/js/custom/printlaporan.js"></script>
+    <script src="/assets/js/custom/dataexport.js?v=1"></script>
+
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.print.min.js"></script>
+    
     <!--end::Page Custom Javascript-->
     <script>
       $(document).ready(function () {
@@ -25,8 +33,25 @@
               "previous": "<",
               "next": ">"
             }
-          }
+          },
+          "dom": 'Bfrtip',
+          "buttons": [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+          ]
         });
       });
+    </script>
+    <script>
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
     </script>
     <!--end::Javascript-->
